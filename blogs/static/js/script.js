63375d359e8ea33daaf02ccccc877886674baee0
Gagandeep Singh <69230886+GaganBTS@users.noreply.github.com>
2023-01-21 11:42:54 +0530

    Delete blogs directory

--- blogs/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// let curs = document.querySelector('.cursor');
-//
-// document.addEventListener('mousemove', (e) => {
-//   let x = e.pageX;
-//   let y = e.pageY;
-//   curs.style.left = (x - 175) + "px";
-//   curs.style.top = (y - 175) + "px";
-//   curs.style.opacity = "1"
-// });
